ca9185aef4ca1a3c7ce90b586c094b6773810bcb
lucas <lucas.gregory@dealersites.com.br>
2019-02-14 10:36:30 -0200

    [Config] Separate config files.

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-print(os.environ)
 app.config.from_object(os.getenv('APP_SETTINGS'))
 db = SQLAlchemy(app)
 
